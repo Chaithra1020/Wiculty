590f74de94065c5ef276224401014d248820bdac
chaithrashree <chaithrasrinivas1020@gmail.com>
2023-07-19 21:43:56 +0530

    merging dev-1.2.4 into master after code freeze based on dev suggestion

--- Login.java
+++ Login.java
@@ -3,7 +3,7 @@ public class LoginUser
       Public static void main(string arg[])
   {
      int i;
-    for(i=10;i<=30;i++)
+    for(i=10;i<=25;i++)
 {
    system.out.print("number:" + i);
        }
