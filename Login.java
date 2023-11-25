ce80e3fddba206ef8b2d8214025013dd6c456031
Chaithrashree <chaithrasrinivas1020@gmail.com>
2023-11-25 11:28:57 +0530

    added a value

--- Login.java
+++ Login.java
@@ -3,7 +3,8 @@ public class LoginUser
       Public static void main(string arg[])
   {
      int i;
-    for(i=10;i<=25;i++)
+    for(i=20;i<=30;i++)
+        
 {
    system.out.print("number:" + i);
        }
